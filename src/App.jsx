6ccc5d74332e5966eb51469477013ab377275762
CruzHumberto <cruzhumberto1@gmail.com>
2024-07-28 11:02:14 -0500

    First commit: creating the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-fuchsia-500">
+        Hola bb, que mas pues (*dice ella con acento paisa)
+      </div>
+    </>
+  );
+}
+
+export default App;
